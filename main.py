413431545b867ef0678ac76b3f6c58bbf45251db
Harrison Conlin <me@harrisony.com>
2020-09-01 16:18:56 +1000

    remove unused logging config

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import os
 import json
 import itertools
 
-from logging.config import dictConfig
-
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
@@ -15,22 +13,6 @@ sentry_sdk.init(
     dsn=os.environ['SENTRY_DSN'],
     integrations=[FlaskIntegration()]
 )
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
 
 app = Flask(__name__)
 
