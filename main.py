74f58b8b4303bb7564f0b3c5624f2b3086c2daaf
Harrison Conlin <me@harrisony.com>
2021-08-17 22:16:06 +1000

    Cases parser now works again after site rebrand

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ ANU_COVID_LEVEL = 'https://www.anu.edu.au/covid-19-advice/campus-community/covid
 ANU_COVID_LEVELS = {'NORMAL', 'LOW', 'MEDIUM', 'HIGH', 'EXTREME',
                     'GREEN', 'BLUE', 'AMBER', 'ORANGE', 'RED',
                     'BLUE PLUS MASKS'}
-ANU_COVID_RISKS = {'GREEN': 'Normal', 'BLUE': 'Low',  'AMBER': 'Medium', 'ORANGE': 'High', 'RED': 'Extreme', 'BLUE PLUS MASKS': 'Low'}
+ANU_COVID_RISKS = {'GREEN': 'Normal', 'BLUE': 'Low', 'AMBER': 'Medium', 'ORANGE': 'High', 'RED': 'Extreme',
+                   'BLUE PLUS MASKS': 'Low'}
+
 
 def process(case):
     app.logger.debug(f"PROCESSING: {case}")
@@ -37,8 +39,8 @@ def process(case):
         app.logger.info(f"Merging strongs: {p_strong}")
         p_strong.pop(0)
         for s in p_strong:
-            case.string += s.get_text() # string should work too
-            s.extract() #decompose causes issues
+            case.string += s.get_text()  # string should work too
+            s.extract()  # decompose causes issues
 
     app.logger.debug(f"Date: {case}")
     casedict = {'date': case.get_text(strip=True)}
@@ -48,7 +50,7 @@ def process(case):
         # details = c.find_next_sibling('p').get_text().strip()
     else:
         app.logger.info("strong embedded in p")
-        case.clear() # clear the strong with the date
+        case.clear()  # clear the strong with the date
         details = case.parent.get_text().strip()
         app.logger.debug(details)
 
@@ -65,6 +67,7 @@ def process(case):
     app.logger.info(casedict)
     return casedict
 
+
 @app.route('/community-cases')
 def handle_news():
     r = requests.get(ANU_COVID_NEWS)
@@ -74,12 +77,28 @@ def handle_news():
     content = page.select_one('[property="content:encoded"]')
     # app.logger.debug(f"BeautifulSoup: {content}")
 
-    last_update = page.select_one('meta[property="article:modified_time"]').get('content')
+    infobox = content.select_one('div.bg-uni25').extract()
+    last_update_meta = page.select_one('meta[property="article:modified_time"]').get('content')
+    last_update = infobox.select_one('h3').text
+
+    ccount = content.select_one('div.bg-uni25').extract()
+
+    case_count = re.match(r'.*(\d+)$', ccount.text.strip()).group(1)
+    app.logger.info(f"number of cases: {ccount}. {case_count}")
+    # cases_heading.parent.decompose()
+    # ccount.decompose()
+
+
+    # Strip the junk
+    # [<h3><strong>Details</strong></h3>,
+    # <h3>2021</h3>,
+    # <h3 class="nounderline"><strong>2020</strong></h3>]
 
     cases_heading = content.select_one('strong')
 
+    # TODO: why is this needed
     for elem in itertools.takewhile(
-        lambda x: cases_heading.parent != x, content.children
+            lambda x: cases_heading.parent != x, content.children
     ):
         if isinstance(elem, NavigableString):
             elem.extract()
@@ -88,23 +107,31 @@ def handle_news():
 
     app.logger.debug(f"case: {cases_heading}")
 
-    ccount = cases_heading.parent.find_next_sibling('p')
-    case_count = re.match(r'.*(\d+)$', ccount.string).group(1)
-    app.logger.info(f"number of cases: {ccount}. {case_count}")
-    cases_heading.parent.decompose()
-    ccount.decompose()
 
-    cases = content.select('strong')
-    # app.logger.debug(f"Selecting rest of cases: {cases}")
+    # Remove Year Headings
+    for e in content.find_all('h3'):
+        if e.text.isnumeric():
+            e.decompose()
+
+    # Remove 2021 nil
+    [x.decompose() for x in content.find_all('p', text='Nil.')]
+
+    #Remove heading now
+    content.select_one('strong', text='Details').decompose()
+
+    cases = list(
+            filter(None.__ne__, (process(xi) for xi in content.select("strong")))
+        )
+
+    app.logger.warn(f"Expected casees: {int(case_count)}. Actual: {len(cases)}")
 
     response = {
-        "last_updated": last_update,
+        "last_updated": last_update_meta,
         "count": int(case_count),
-        "cases": list(
-            filter(None.__ne__, (process(xi) for xi in content.select("strong")))
-        ),
+        "cases": cases,
     }
     return jsonify(response)
+
 
 @app.route('/alert-level')
 def process_alert():
@@ -130,5 +157,3 @@ def process_alert():
         raise Exception("COVIDSafe Campus Alert level error", level)
 
     return jsonify(alert_level=level.title(), risk=risk, last_updated=last_update, details=box.text.strip())
-
-
