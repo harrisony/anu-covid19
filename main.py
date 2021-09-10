cfc3e160c731d20cd7a6506fd862b79ffaa90682
Harrison Conlin <me@harrisony.com>
2021-09-10 16:19:01 +1000

    ANU removed the details of positive cases

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import itertools
 import copy
 
 import sentry_sdk
+from json2xml import json2xml
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 sentry_sdk.init(
@@ -29,77 +30,6 @@ ANU_COVIDSAFE_RISKS = {'GREEN': 'Normal', 'BLUE': 'Low', 'AMBER': 'Medium', 'ORA
                    'BLUE PLUS MASKS': 'Low'}
 
 
-def process_2020(case):
-    app.logger.debug(f"PROCESSING: {case}")
-    app.logger.debug(f"\twith parent {case.parent}")
-
-    if case.parent is None:  # This occurs with the 'strong p'
-        return
-
-    p_strong = case.parent.find_all('strong')
-    if len(p_strong) > 1:
-        app.logger.info(f"Merging strongs: {p_strong}")
-        p_strong.pop(0)
-        for s in p_strong:
-            case.string += s.get_text()  # string should work too
-            s.extract()  # decompose causes issues
-
-    app.logger.debug(f"Date: {case}")
-    casedict = {'date': case.get_text(strip=True)}
-    if case.get_text() == case.parent.get_text():
-        app.logger.debug("normal details extraction")
-        details = case.parent.find_next_sibling('p').get_text().strip()
-        # details = c.find_next_sibling('p').get_text().strip()
-    else:
-        app.logger.info("strong embedded in p")
-        case.clear()  # clear the strong with the date
-        details = case.parent.get_text().strip()
-        app.logger.debug(details)
-
-    # This hurts...but so does COVID.
-    # Updates casedict with the details of the case (who, what, when, where, why, etc)
-    # They're in the form `Actions: Contract tracing and separated by new lines
-
-    casedict = dict(casedict, **dict(
-        [(i[0].strip().replace(' ', '_'), i[1].strip()) for i in (p.split(':') for p in details.split('\n'))]
-    ))
-
-    # As at 9 Apr, they seem to skip Actions and just have further details
-
-    app.logger.info(casedict)
-    return casedict
-
-def process(cases):
-
-    date_cases = []
-    date = ''
-    for case in cases.find_all('p'):
-        if len(case.get_text(strip=True)) == 0:
-            case.decompose()
-            continue
-        try:
-            date = datetime.datetime.strptime(case.get_text(strip=True), "%d %B %Y")
-            continue
-        except ValueError:
-            app.logger.debug(f"content: {case}")
-            pass
-        app.logger.debug(f"Date: {date}")
-        casedict = {'date': date.strftime("%d %B %Y")}
-        details = case.get_text().strip()
-        # This hurts...but so does COVID.
-        # Updates casedict with the details of the case (who, what, when, where, why, etc)
-        # They're in the form `Actions: Contract tracing and separated by new lines
-
-        casedict = dict(casedict, **dict(
-            [(i[0].strip().replace(' ', '_'), i[1].strip()) for i in (p.split(':') for p in details.split('\n'))]
-        ))
-        date_cases.append(casedict)
-
-    # As at 9 Apr, they seem to skip Actions and just have further details
-
-    return date_cases
-
-
 @app.route('/community-cases')
 def handle_news():
     r = requests.get(ANU_COVID_NEWS)
@@ -113,48 +43,18 @@ def handle_news():
     last_update_meta = page.select_one('meta[property="article:modified_time"]').get('content')
     last_update = infobox.select_one('h3').text
 
-    ccount = content.select_one('div.bg-uni25').extract()
+    ccount = content.select('div.box-solid strong')
 
-    case_count = re.search(r'(\d+)$', ccount.text.strip()).group(0)
-    app.logger.info(f"number of cases: {ccount.get_text(strip=True)}. {case_count}")
-    # cases_heading.parent.decompose()
-    # ccount.decompose()
-
-    cases_heading = content.select_one('strong')
-
-    # TODO: why is this needed
-    for elem in itertools.takewhile(
-            lambda x: cases_heading.parent != x, content.children
-    ):
-        if isinstance(elem, NavigableString):
-            elem.extract()
-        else:
-            elem.decompose()
-
-    app.logger.debug(f"case: {cases_heading}")
-
-    # Remove heading now
-    content.select_one('strong', text='Details').parent.decompose()
-
-    c_2021 = copy.copy(content)
-    [e.decompose() for e in c_2021.find('h3', text='2020').previous_sibling.find_next_siblings()]
-    c_2021.find('h3', text='2021').decompose()
-    cases = process(c_2021)
-
-    c_2020 = copy.copy(content)
-    [e.decompose() for e in c_2020.find('h3', text='2020').find_previous_siblings()]
-    c_2020.select_one('h3').decompose()
-    cases.extend(
-        filter(None.__ne__, (process_2020(xi) for xi in c_2020.select("strong")))
-    )
-
-    app.logger.warn(f"Expected casees: {int(case_count)}. Actual: {len(cases)}")
+    #Total number of reported ANU cases in the ACT: 3
+    #Total number of reported ANU cases outside of the ACT: 5
+    act = re.search(r'(\d+)', ccount[0].text.strip()).group(0)
+    outside = re.search(r'(\d+)', ccount[1].text.strip()).group(0)
 
     response = {
         "last_updated_meta": last_update_meta,
         "last_updated_official": last_update,
-        "count": int(case_count),
-        "cases": cases,
+        "count": int(act),
+        "outside": int(outside)
     }
     return jsonify(response)
 
@@ -222,3 +122,12 @@ def latest_anuobserver():
     for post in r.json():
         if 'covid' in post['slug']:
             return redirect(post['link'])
+
+
+
+
+@app.route('/chs-community-checkin-news')
+def checkin_news():
+    r = requests.get('https://chs-community-checkin-api.azurefd.net/api/v1/content', headers={'x-api-key': 'bm5VTzh0SkZ6b1ZGVzVEb3hSSmNpbEN0Tjdvd0l6ZDE='})
+    d = r.json()
+    return json2xml.Json2xml(d).to_xml()
