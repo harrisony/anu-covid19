65faac28037ada7b7a560dd628b41d4617e912c7
Harrison Conlin <me@harrisony.com>
2022-03-25 12:09:06 +1100

    why they gotta keep changing things;

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ sentry_sdk.init(
 
 app = Flask(__name__)
 
-ANU_COVIDSAFE_LEVEL = 'https://www.anu.edu.au/covid-19-advice/how-were-responding-to-covid-19/campus-alert-system/campus-alert-system'
-ANU_RESIDENCE_LEVEL = 'https://www.anu.edu.au/covid-19-advice/how-were-responding-to-covid-19/information-for-students/residential-students-on'
+ANU_COVIDSAFE_LEVEL = 'https://www.anu.edu.au/covid-19-advice'
 
 ANU_COVIDSAFE_LEVELS = {'NORMAL', 'LOW', 'MEDIUM', 'HIGH', 'EXTREME'}
 
@@ -42,10 +41,8 @@ def process_alert():
     content = BeautifulSoup(r.text, features="html.parser")
 
     last_update = content.select_one('meta[property="article:modified_time"]').get('content')
-    box = content.select_one('[property="content:encoded"]').select_one('div')
+    box = content.find('h2', text="Current alert level").parent
 
-    if box.select_one('h2').text != "Current alert level":
-        app.logger.warn("page has changed again?", box)
     box.select_one('h2').decompose()
 
     level = alert_new(box)
@@ -55,14 +52,15 @@ def process_alert():
 
     return jsonify(alert_level=level.title(), last_updated=last_update, details=box.text.strip())
 
-@app.route('/latest-anuobserver-live')
-def latest_anuobserver():
-    CATEGORY = 'https://anuobserver.org/wp-json/wp/v2/posts?categories=306'
-    r = requests.get(CATEGORY)
-    for post in r.json():
-        if 'covid' in post['slug']:
-            return redirect(post['link'])
+@app.route('/latest-anu-updates')
+def latest_anu_updates():
+    r = requests.get(ANU_COVID_SAFE_LEVEL)
+    content = BeautifulSoup(r.text, features="html.parser")
+    last_update = content.select_one('meta[property="article:modified_time"]').get('content')
+    box = content.find('h2', text="Latest updates").parent
+    box.select_one('h2').decompose()
 
+    return jsonify(last_updated=last_update, details=box.text.replace(u"\u00A0", " ").strip())
 
 
 
